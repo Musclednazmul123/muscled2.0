84a89bcdf675ed7a1f70e72c201724fbeaa9631c
Musclednazmul123 <nazmul.muscled@gmail.com>
2023-02-06 21:57:19 +0600

    project update

--- app/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '6f6fda12a56615bcf9d42ed2a7895f00'),
-    },
-  },
-});

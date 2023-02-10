82c19a6c4a113cfd0e0d92d43b8c60abe2c0455c
Musclednazmul123 <nazmul.muscled@gmail.com>
2023-02-10 18:52:09 +0600

    db update

--- strapi/config/database.js
+++ strapi/config/database.js
@@ -4,10 +4,38 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'postgres'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'Loveinthetimeofcolera1.'),
-      ssl: env.bool('DATABASE_SSL', false),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      schema: env('DATABASE_SCHEMA', 'public'), // Not required
+      ssl: env('DATABASE_SSL', false)
     },
+    acquireConnectionTimeout: 1000000,
+    pool: {
+      min: 0,
+      max: 4,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 300000,
+      idleTimeoutMillis: 30000,
+      reapIntervalMillis:1000,
+      createRetryIntervalMillis: 2000
+    },
+    debug: false,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'postgres',
+//     connection: {
+//       host: env('DATABASE_HOST', 'localhost'),
+//       port: env.int('DATABASE_PORT', 5432),
+//       database: env('DATABASE_NAME', 'postgres'),
+//       user: env('DATABASE_USERNAME', 'postgres'),
+//       password: env('DATABASE_PASSWORD', 'Loveinthetimeofcolera1.'),
+//       ssl: env.bool('DATABASE_SSL', false),
+//     },
+//   },
+// });
+
